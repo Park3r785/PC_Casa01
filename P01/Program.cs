ce97e9c9721ea7fda1abd682bc94ea67207d0942
Park3r785 <moranoteroangello@gmail.com>
2025-04-09 18:07:32 -0500

    Segundo commit

--- P01/Program.cs
+++ P01/Program.cs
@@ -11,14 +11,23 @@ namespace P01
         static void Main(string[] args)
         {
             E1();
+            E2();
             Console.ReadKey();
         }
         static void E1()
         {
             const int r = 5;
             double a;
-            a = Math.PI * Math.Pow(r,2);
-            Console.WriteLine("El área del círculo sería: "+ a );
+            a = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine("El área del círculo sería: " + a);
+        }
+        static void E2()
+        {
+            double catetoA = 3;
+            double catetoB = 4;
+            double hipotenusa = Math.Sqrt(Math.Pow(catetoA, 2) + Math.Pow(catetoB, 2));
+            // h = sqrt(a^2 + b^2)
+            Console.WriteLine("Longitud de la hipotenusa: " + hipotenusa);
         }
         }
     }
