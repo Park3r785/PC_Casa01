4dbaf3aa9e36967e9071f1a89482310e4b18c90a
Park3r785 <moranoteroangello@gmail.com>
2025-04-09 17:54:34 -0500

    Primer commit

---
+++ P01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            E1();
+            Console.ReadKey();
+        }
+        static void E1()
+        {
+            const int r = 5;
+            double a;
+            a = Math.PI * Math.Pow(r,2);
+            Console.WriteLine("El área del círculo sería: "+ a );
+        }
+        }
+    }
